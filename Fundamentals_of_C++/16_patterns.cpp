b9c8e7bb393cb97d354c92423c3dba7f86fa080c
Srinivasan <64957668+Srinivsan-m@users.noreply.github.com>
2020-05-18 20:55:28 +0530

    Day 6 more Patterns added

--- Fundamentals_of_C++/16_patterns.cpp
+++ Fundamentals_of_C++/16_patterns.cpp
@@ -33,4 +33,53 @@ int main()
         }
         cout << '\n';
     }
+    cout << "\n\n";
+
+    // number pyramid
+    int count = 0, count2 = 0, k = 0;
+    for (int i = 1; i <= rows; i++)
+    {
+        for (int space = 1; space <= rows - i; space++)
+        {
+            cout << "  ";
+            count++;
+        }
+
+        while (k != 2 * i - 1)
+        {
+            if (count <= rows - 1)
+            {
+                cout << i + k << " ";
+                count++;
+            }
+            else
+            {
+                count2++;
+                cout << i + k - 2 * count2 << " ";
+            }
+            k++;
+        }
+        count = count2 = k = 0;
+        cout << '\n';
+    }
+    cout << "\n\n";
+
+    // Pascal's triangle
+    int coef = 1;
+    for (int i = 0; i < rows; i++)
+    {
+        for (int space = 1; space <= rows - i; space++)
+            cout << "  ";
+
+        for (int j = 0; j <= i; j++)
+        {
+            if (j == 0 || i == 0)
+                coef = 1;
+            else
+                coef = coef * (i - j + 1) / j;
+
+            cout << coef << "   ";
+        }
+        cout << '\n';
+    }
 }
