4a3fd20d0eb11ec8ad0878fc18c9e6710a261ce2
Srinivasan <64957668+Srinivsan-m@users.noreply.github.com>
2020-06-02 17:20:14 +0530

    Add files via upload

---
+++ Arduinio_digital_clock/Arduinio_digital_clock.ino
@@ -0,0 +1,58 @@
+#include <SPI.h>
+#include <Wire.h>
+#include <Adafruit_GFX.h>
+#include <Adafruit_SSD1306.h>
+
+#include <Wire.h>
+#include <TimeLib.h>
+#include <DS1307RTC.h>
+
+#define SCREEN_WIDTH 128 // OLED display width, in pixels
+#define SCREEN_HEIGHT 64 // OLED display height, in pixels
+
+// Declaration for SSD1306 display connected using software SPI (default case):
+#define OLED_MOSI   9 //D1
+#define OLED_CLK   10 //D0
+#define OLED_DC    11 //DC
+#define OLED_CS    12 //CS
+#define OLED_RESET 13 //RES
+Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT,
+  OLED_MOSI, OLED_CLK, OLED_DC, OLED_RESET, OLED_CS);
+
+void setup() {
+  Serial.begin(9600);
+
+  // SSD1306_SWITCHCAPVCC = generate display voltage from 3.3V internally
+  if(!display.begin(SSD1306_SWITCHCAPVCC)) {
+    Serial.println(F("SSD1306 allocation failed"));
+    for(;;); // Don't proceed, loop forever
+  }
+  // Pause for 2 seconds
+  delay(2000); 
+  
+  // Clear the buffer
+  display.clearDisplay();
+  display.setTextSize(2);      // Normal 1:1 pixel scale
+  display.setTextColor(SSD1306_WHITE); // Draw white text
+  display.setCursor(0, 0);     // Start at top-left corner
+}
+
+void loop() {
+  tmElements_t tm;
+  
+  if (RTC.read(tm)) {
+    display.println(""+String(tm.Hour)+" : "+String(tm.Minute)+" : "+String(tm.Second)+""); 
+  } else {
+    if (RTC.chipPresent()) {
+      Serial.println("The DS1307 is stopped.  Please run the SetTime");
+      Serial.println("example to initialize the time and begin running.");
+      Serial.println();
+    } else {
+      Serial.println("DS1307 read error!  Please check the circuitry.");
+      Serial.println();
+    }
+    delay(9000);
+  }
+  delay(1000);
+  
+}
